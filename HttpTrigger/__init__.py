9853937d909f26d7936ca0a957a62754d38dda2e
ftfly <ftfly@126.com>
2019-09-12 15:16:57 +0800

    Update __init__.py

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -13,9 +13,9 @@ from .conversion.excel2csv import Excel2Csv
 def main(req: func.HttpRequest) -> func.HttpResponse:
     credentials = MSIAuthentication(resource='https://vault.azure.net')
     kvclient = KeyVaultClient(credentials)
-    key = kvclient.get_secret("https://zhenzh-databricks-kv.vault.azure.net/", "zhenzh-python-func-account-key", "").value
+    key = kvclient.get_secret("https://mnanalyticssandbox-vault.vault.azure.net/", "sakey", "").value
     
-    converter = Excel2Csv(BlockBlobService(account_name='zhenzhadfblobsource', account_key=key))
+    converter = Excel2Csv(BlockBlobService(account_name='xlsxupload', account_key=key))
     converter.convert_and_upload()         
     
     return json.dumps({"result": "Conversion Finished!"})
